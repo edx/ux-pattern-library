6773c15f1c5e897e1b453926834dd2f5fe4ca57f
Chris Rodriguez <crodriguez@edx.org>
2015-04-28 14:04:03 -0400

    Reducing clutter, rearranging script compile

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -8,12 +8,13 @@ var uglify          = require('gulp-uglify');
 gulp.task('scripts', ['pl_scripts-lint'], function() {
     return gulp.src([
             // setup script sequence
+            './_src/vendor/jquery/jquery-2.1.3.min.js',
             './_src/vendor/svg4everybody/svg4everybody.min.js' // polyfill for SVGs in IE9-11
         ])
-        .pipe(concat('vendor.js'))
+        .pipe(concat('main.js'))
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(config.pl_local)) // move just for browersync + local preview
-        .pipe(gulp.dest(config.pl_dest))
+        .pipe(gulp.dest(config.local)) // move just for browersync + local preview
+        .pipe(gulp.dest(config.dest))
         .pipe(browserSync.reload({stream:true}))
 });
