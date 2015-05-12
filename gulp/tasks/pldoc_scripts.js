ff89af03b4af36da3ae7a322fc12e5cd6ea27b7c
Chris Rodriguez <crodriguez@edx.org>
2015-05-12 16:45:07 -0400

    Elements - Forms

--- gulp/tasks/pldoc_scripts.js
+++ gulp/tasks/pldoc_scripts.js
@@ -2,21 +2,18 @@ var gulp            = require('gulp');
 var config          = require('../config').scripts;
 var browserSync     = require('browser-sync');
 var concat          = require('gulp-concat');
-var rename          = require('gulp-rename');
 var uglify          = require('gulp-uglify');
 
 gulp.task('pldoc_scripts', ['pldoc_scripts-lint'], function() {
     return gulp.src([
             // setup script sequence
-            './_src/pldoc/vendor/jquery/jquery.smooth-scroll.js',
-            config.pldoc_src + '/ui.js',
-            config.pldoc_src + '/size-slider.js',
-            config.pldoc_src + '/color-contrast.js',
-            './_src/vendor/svg4everybody/svg4everybody.min.js' // polyfill for SVGs in IE9-11
+            './_src/pldoc/vendor/jquery-smoothScroll/jquery.smooth-scroll.js',
+            './_src/pldoc/static/js/ui.js',
+            './_src/pldoc/static/js/size-slider.js',
+            './_src/pldoc/static/js/color-contrast.js'
         ])
-        .pipe(concat('main.js'))
+        .pipe(concat('pldoc.js'))
         .pipe(uglify())
-        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest(config.pldoc_local)) // move just for browersync + local preview
         .pipe(gulp.dest(config.pldoc_dest))
         .pipe(browserSync.reload({stream:true}))
