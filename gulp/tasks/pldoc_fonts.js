781d1e5554b7a1aef717e26e1fa6cf5ea07432a7
Chris Rodriguez <github@chrisrodriguez.me>
2015-06-11 13:14:56 -0400

    Some cleanup, WIP

--- gulp/tasks/pldoc_fonts.js
+++ gulp/tasks/pldoc_fonts.js
@@ -5,9 +5,8 @@ var browserSync     = require('browser-sync');
 gulp.task('pldoc_fonts', function() {
     return gulp.src([
             // move fonts to public
-            './_src/pldoc/static/fonts/OpenSans/**'
+            './_src/patter-library/fonts/OpenSans/**'
         ])
-        .pipe(gulp.dest('./_site/public/pldoc/fonts/OpenSans/')) // move just for browersync + local preview
         .pipe(gulp.dest('./public/pldoc/fonts/OpenSans/'))
         .pipe(browserSync.reload({stream:true}))
 });
