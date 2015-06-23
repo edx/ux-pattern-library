ebc40337f7105423ae87f4c25ce726b213112256
Brian Talbot <hello.talbs@gmail.com>
2015-06-23 13:46:44 -0400

    Revert "Adding and configuring RequireJS in the Pattern Library"

--- gulp/tasks/pldoc_scripts.js
+++ gulp/tasks/pldoc_scripts.js
@@ -1,18 +1,18 @@
 var gulp            = require('gulp');
 var config          = require('../config').scripts;
 var browserSync     = require('browser-sync');
+var concat          = require('gulp-concat');
 var uglify          = require('gulp-uglify');
 
 gulp.task('pldoc_scripts', ['pldoc_scripts-lint'], function() {
     return gulp.src([
             // setup script sequence
             './_src/pldoc/vendor/jquery-smoothScroll/jquery.smooth-scroll.js',
-            config.pldoc_src + '/required.js',
-            config.pldoc_src + '/ui.js',
-            config.pldoc_src + '/tabs.js',
-            config.pldoc_src + '/size-slider.js',
-            config.pldoc_src + '/color-contrast.js'
+            './_src/pldoc/static/js/ui.js',
+            './_src/pldoc/static/js/size-slider.js',
+            './_src/pldoc/static/js/color-contrast.js'
         ])
+        .pipe(concat('pldoc.js'))
         .pipe(uglify())
         .pipe(gulp.dest(config.pldoc_local)) // move just for browersync + local preview
         .pipe(gulp.dest(config.pldoc_dest))
