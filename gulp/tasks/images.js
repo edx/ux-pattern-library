e14d77f589fd5ff525e6a7ea1537d29b5afb58b3
Brian Talbot <btalbot@edx.org>
2015-09-04 07:27:52 -0400

    pldoc: revising/fixing image optimization workflow
    * cefdc9e pldoc: revising image-optimization tasks:
    * a2d9a5b pldoc: FIX - correcting image-based paths in gulp config
    * fc6d6c1 moving SVG-vased icons to icons dir
    * 07d935f pldoc: separating image-based tasks in gulp watch file
    * a9274a2 fixup! fixing pldoc image task
    * 536e380 fixup! correct pl images dest path
    * 2d2bd72 fixup! processing images

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,14 +1,18 @@
-var gulp          = require('gulp'),
-    config        = require('../config').images,
-    changed       = require('gulp-changed'),
-    imagemin      = require('gulp-imagemin'),
-    browserSync   = require('browser-sync');
+var gulp            = require('gulp'),
+    config          = require('../config').images,
+    changed         = require('gulp-changed'),
+    imagemin        = require('gulp-imagemin'),
+    pngquant        = require('imagemin-pngquant'),
+    browserSync     = require('browser-sync');
 
 gulp.task('images', function() {
     return gulp.src(config.src_files)
-        .pipe(changed(config.dest)) // Ignore unchanged files
-        .pipe(imagemin()) // Optimize
+        .pipe(changed(config.dest)) // ignore unchanged files
+        .pipe(imagemin({            // optimize
+            progressive: true,
+            use: [pngquant()]
+         }))
+        .pipe(gulp.dest(config.dest))
         .pipe(gulp.dest(config.local)) // move just for browersync + uncompressed local
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest(config.dest));
+        .pipe(browserSync.reload({stream:true}));
 });
