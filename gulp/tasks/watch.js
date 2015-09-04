04ba31c6c242d816ca094d3d8fb89d3a998cc5c8
Brian Talbot <btalbot@edx.org>
2015-09-04 07:27:52 -0400

    pldoc: revising/fixing image optimization workflow
    * cefdc9e pldoc: revising image-optimization tasks:
    * a2d9a5b pldoc: FIX - correcting image-based paths in gulp config
    * fc6d6c1 moving SVG-vased icons to icons dir
    * 07d935f pldoc: separating image-based tasks in gulp watch file
    * a9274a2 fixup! fixing pldoc image task
    * 536e380 fixup! correct pl images dest path
    * 2d2bd72 fixup! processing images

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,7 +5,8 @@ gulp.task('watch', ['browserSync'], function() {
 
     // patterns and documentation site
     gulp.watch([config.styles.pldoc_src_files, config.styles.src_files], ['pldoc_styles']);
-    gulp.watch([config.images.pldoc_src_files, config.images.src_files], ['pldoc_images']);
+    gulp.watch(config.images.src_files, ['images']);
+    gulp.watch(config.images.pldoc_src_files, ['pldoc_images']);
     gulp.watch(config.scripts.pldoc_src_files, ['pldoc_scripts']);
 
     // local
