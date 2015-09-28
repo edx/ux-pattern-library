197dccef67c7998018315b2696eb176a18ee56fd
Brian Talbot <btalbot@edx.org>
2015-09-28 12:19:56 -0400

    pldoc: adding in font-based gulp task/workflow

---
+++ gulp/tasks/fonts.js
@@ -0,0 +1,17 @@
+var gulp            = require('gulp'),
+    browserSync     = require('browser-sync'),
+    changed         = require('gulp-changed'),
+    config          = require('../config').fonts,
+    merge           = require('merge-stream');
+
+gulp.task('fonts', function() {
+
+    var fonts = gulp.src(config.src_files);
+    var pldoc_fonts = gulp.src(config.pldoc_src_files);
+
+    return merge(fonts, pldoc_fonts)
+        .pipe(changed(config.dest)) // ignore unchanged files
+        .pipe(gulp.dest(config.local)) // move just for browersync + uncompressed local
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest(config.dest))
+});
