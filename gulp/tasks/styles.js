0c4f73fe0a07b150f4ba7d53c0f40a8c65df6f3e
Brian Talbot <btalbot@edx.org>
2015-09-30 20:42:58 -0400

    pldoc: simplifying gulp styles task:
    * minification now handled in build-production
    * no production-settings needed/used, so those have been removed

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,21 +1,17 @@
 var gulp            = require('gulp'),
     autoprefixer    = require('gulp-autoprefixer'),
     browserSync     = require('browser-sync'),
-    changed         = require('gulp-changed'),
     config          = require('../config').styles,
     handleErrors    = require('../util/handleErrors'),
-    minifyCSS       = require('gulp-minify-css'),
     sass            = require('gulp-sass'),
     sourcemaps      = require('gulp-sourcemaps');
 
 gulp.task('styles', function () {
     return gulp.src(config.pldoc_src_files)
         .pipe(sourcemaps.init())
-        .pipe(changed(config.dest)) // ignore unchanged files
         .pipe(sass(config.settings_develepment))
         .on('error', handleErrors)
         .pipe(autoprefixer())
-        .pipe(minifyCSS())
         .pipe(sourcemaps.write(config.settings_development.sourcemapsLocation))
         .pipe(gulp.dest(config.local)) // move just for browersync + uncompressed local
         .pipe(browserSync.reload({stream:true}))
