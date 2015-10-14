e4570f7c9d0f24c66036b586b3bacd961bf274b9
Dennis Jen <djen@edx.org>
2015-10-14 12:54:30 -0400

    added start-collapsible main

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -4,9 +4,18 @@ var gulp            = require('gulp'),
     uglify          = require('gulp-uglify');
 
 gulp.task('scripts', ['scripts-lint'], function() {
+    'use strict';
+
+    // TODO:
+    // 1. refactor so that these can be watched & uglified, etc.
+    // 2. refactor other bower components...
+    gulp.src([config.lib.src + '/**/*'])
+        .pipe(gulp.dest(config.scripts.local + '/bower_components'))
+        .pipe(gulp.dest(config.scripts.dest + '/bower_components'));
 
     return gulp.src([
             // setup script sequence
+//            config.lib.src + '/**/*',
             config.lib.src + '/requirejs/require.js',
             config.lib.src + '/jquery/dist/jquery.min.js',
             config.lib.src + '/svg4everybody/svg4everybody.min.js',
@@ -17,7 +26,7 @@ gulp.task('scripts', ['scripts-lint'], function() {
             config.scripts.src + '/testing.js'
         ])
         .pipe(uglify())
-        .pipe(gulp.dest(config.scripts.local)) // move for just browsercync + local preview
+        .pipe(gulp.dest(config.scripts.local)) // move for just browsersync + local preview
         .pipe(browserSync.reload({ stream: true }))
-        .pipe(gulp.dest(config.scripts.dest))
+        .pipe(gulp.dest(config.scripts.dest));
 });
