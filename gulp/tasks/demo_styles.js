ea52497185e0b7fd25223b1ee0e59bb868141a6a
Brian Talbot <btalbot@edx.org>
2015-12-08 15:59:53 -0500

    pldoc: adding in example empty development/prototyping area (demo)

---
+++ gulp/tasks/demo_styles.js
@@ -0,0 +1,19 @@
+var gulp            = require('gulp'),
+    autoprefixer    = require('gulp-autoprefixer'),
+    browserSync     = require('browser-sync'),
+    config          = require('../config').styles,
+    handleErrors    = require('../util/handleErrors'),
+    sass            = require('gulp-sass'),
+    sourcemaps      = require('gulp-sourcemaps');
+
+gulp.task('demo_styles', function () {
+    return gulp.src(config.demo_src_files)
+        .pipe(sourcemaps.init())
+        .pipe(sass(config.settings_development))
+        .on('error', handleErrors)
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write(config.settings_development.sourcemapsLocation))
+        .pipe(gulp.dest(config.demo_local)) // move just for browersync + uncompressed local
+        .pipe(gulp.dest(config.demo_dest))
+        .pipe(browserSync.reload({stream:true}));
+});
--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -8,8 +8,9 @@ gulp.task('fonts', function() {
 
     var fonts = gulp.src(config.src_files);
     var pldoc_fonts = gulp.src(config.pldoc_src_files);
+    var demo_fonts = gulp.src(config.demo_src_files);
 
-    return merge(fonts, pldoc_fonts)
+    return merge(fonts, pldoc_fonts, demo_fonts)
         .pipe(changed(config.dest)) // ignore unchanged files
         .pipe(gulp.dest(config.local)) // move just for browersync + uncompressed local
         .pipe(browserSync.reload({stream:true}))
