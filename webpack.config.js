759395de20ffc3105d65b60bee3580cbf3702778
Andy Armstrong <andya@edx.org>
2016-03-01 09:19:27 -0500

    Bundle assets using WebPack

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,25 @@
     'use strict';
 
     var path = require('path'),
+<<<<<<< 99ec845825259e19353a5b3855ff6368db3b1705
         webpack = require('webpack'),
         outputRoot = process.env.OUTPUT_ROOT !== undefined ? process.env.OUTPUT_ROOT : 'public',
         siteRoot = process.env.SITE_ROOT !== undefined ? process.env.SITE_ROOT : '/',
         publicJavaScriptRoot = 'public/pldoc/js/';
+=======
+        webpack = require('webpack');
+>>>>>>> Bundle assets using WebPack
 
     module.exports = {
         entry: path.resolve(__dirname, 'pldoc/static/js/pattern-library.js'),
         output: {
+<<<<<<< 99ec845825259e19353a5b3855ff6368db3b1705
             path: path.resolve(__dirname, outputRoot + publicJavaScriptRoot),
             publicPath: siteRoot + publicJavaScriptRoot,
+=======
+            path: path.resolve(__dirname, 'public'),
+            publicPath: '/public/pldoc/js/',
+>>>>>>> Bundle assets using WebPack
             filename: 'pattern-library.js'
         },
         modulesDirectories: ['node_modules'],
