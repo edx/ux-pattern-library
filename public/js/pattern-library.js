502a79e353dcb8e94a1831dbcf15cd60dabae31c
Andy Armstrong <andya@edx.org>
2016-03-01 14:48:00 -0500

    Update public site with most recent changes

--- public/js/pattern-library.js
+++ public/js/pattern-library.js
@@ -1,1 +1,1 @@
-require.config({baseUrl:"/public/js",paths:{jquery:"/public/js/jquery.min",modernizr:"/public/js/modernizr-custom",afontgarde:"/public/js/afontgarde",edxicons:"/public/js/edx-icons"},shim:{jquery:{exports:"jquery"},afontgarde:{exports:"AFontGarde"}}}),require(["jquery","/public/js/ui.js","/public/js/modernizr-custom.js","afontgarde","/public/js/edx-icons.js"],function(e,r){});
+require(["jquery","./ui.js","pattern-library/js/modernizr-custom","pattern-library/js/afontgarde","pattern-library/js/edx-icons"],function(r,e){});
