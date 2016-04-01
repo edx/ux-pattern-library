e526019e7ef191673339ce9935f2c19a6b63227e
Andy Armstrong <andya@edx.org>
2016-03-31 23:28:11 -0400

    Clean up library imports

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,18 +1,23 @@
-var gulp            = require('gulp'),
-    autoprefixer    = require('gulp-autoprefixer'),
-    browserSync     = require('browser-sync'),
-    config          = require('../config').styles,
-    handleErrors    = require('../util/handleErrors'),
-    sass            = require('gulp-sass'),
-    sourcemaps      = require('gulp-sourcemaps');
+(function() {
+    'use strict';
 
-gulp.task('styles', function () {
-    return gulp.src(config.src_files)
-        .pipe(sourcemaps.init())
-        .pipe(sass(config.settings_develepment))
-        .on('error', handleErrors)
-        .pipe(autoprefixer())
-        .pipe(sourcemaps.write(config.settings_development.sourcemapsLocation))
-        .pipe(gulp.dest(config.dest))
-        .pipe(browserSync.reload({stream:true}));
-});
+    var gulp            = require('gulp'),
+        autoprefixer    = require('gulp-autoprefixer'),
+        browserSync     = require('browser-sync'),
+        config          = require('../config').styles,
+        handleErrors    = require('../util/handleErrors'),
+        sass            = require('gulp-sass'),
+        sourcemaps      = require('gulp-sourcemaps');
+
+    gulp.task('styles', function() {
+        return gulp.src(config.src_files)
+            .pipe(sourcemaps.init())
+            .pipe(sass(config.settings_development))
+            .on('error', handleErrors)
+            .pipe(autoprefixer())
+            .pipe(sourcemaps.write(config.settings_development.sourcemapsLocation))
+            .pipe(gulp.dest(config.dest))
+            .pipe(browserSync.reload({stream: true}));
+    });
+})();
+
