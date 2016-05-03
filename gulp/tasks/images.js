83d0dc9b1d9af96300038f0b97fcdd8e306b3d97
Andy Armstrong <andya@edx.org>
2016-05-03 14:47:50 -0400

    Remove imagemin from the gulp pipeline
    FEDX-178

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,24 +1,20 @@
+'use strict';
+
 var gulp            = require('gulp'),
     browserSync     = require('browser-sync'),
     changed         = require('gulp-changed'),
     config          = require('../config').images,
-    imagemin        = require('gulp-imagemin'),
-    merge           = require('merge-stream'),
-    pngquant        = require('imagemin-pngquant');
+    merge           = require('merge-stream');
 
 gulp.task('images', function() {
 
-    var images = gulp.src(config.src_files);
-    var pldoc_images = gulp.src(config.pldoc_src_files);
-    var demo_images = gulp.src(config.demo_src_files);
+    var images = gulp.src(config.src_files),
+        pldocImages = gulp.src(config.pldoc_src_files),
+        demoImages = gulp.src(config.demo_src_files);
 
-    return merge(images, pldoc_images, demo_images)
+    return merge(images, pldocImages, demoImages)
         .pipe(changed(config.dest)) // ignore unchanged files
-        .pipe(imagemin({            // optimize
-            progressive: true,
-            use: [pngquant()]
-         }))
         .pipe(gulp.dest(config.local)) // move just for browersync + uncompressed local
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest(config.dest))
+        .pipe(browserSync.reload({stream: true}))
+        .pipe(gulp.dest(config.dest));
 });
