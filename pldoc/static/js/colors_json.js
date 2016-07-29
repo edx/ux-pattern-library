e9613002ed222cfbdcc321781b70c20735112e47
Chris Rodriguez <github@chrisrodriguez.me>
2016-07-29 09:14:29 -0400

    Cleanup after a time-sensitive update

--- pldoc/static/js/colors_json.js
+++ pldoc/static/js/colors_json.js
@@ -8,35 +8,35 @@ define(function() {
                 title: 'Primary Dark',
                 description: 'The darkest shade of the primary color.',
                 recs: [{
-                    back: ['white', 'light', 'x-light']
+                    back: ['white', 'back', 'x-back']
                 }]
             }],
             base: [{
                 title: 'Primary Base',
                 description: 'The base primary color intended for main use.',
                 recs: [{
-                    back: ['white', 'light', 'x-light']
+                    back: ['white', 'back', 'x-back']
                 }]
             }],
             accent: [{
                 title: 'Accent Primary',
                 description: 'The primary accent color.',
                 recs: [{
-                    back: ['white', 'light', 'x-light']
+                    back: ['white', 'back', 'x-back']
                 }]
             }],
             back: [{
                 title: 'Primary Background',
                 description: 'A lighter tint of the primary base color used for backgrounds.',
                 recs: [{
-                    back: ['dark', 'x-dark', 'black']
+                    back: ['dark', 'black']
                 }]
             }],
             'x-back': [{
                 title: 'Primary Lighter Background',
                 description: 'A very light, near white, tint of the primary base color for lighter backgrounds.',
                 recs: [{
-                    back: ['dark', 'x-dark', 'black']
+                    back: ['dark', 'black']
                 }]
             }]
         }, {
@@ -45,35 +45,35 @@ define(function() {
                 title: 'Secondary Dark',
                 description: 'The darkest shade of the secondary color.',
                 recs: [{
-                    back: ['white', 'light', 'x-light']
+                    back: ['white', 'back', 'x-back']
                 }]
             }],
             base: [{
                 title: 'Secondary Base',
                 description: 'The base secondary color intended for main use.',
                 recs: [{
-                    back: ['white', 'light', 'x-light']
+                    back: ['white', 'back', 'x-back']
                 }]
             }],
             accent: [{
                 title: 'Accent Secondary',
                 description: 'The secondary accent color.',
                 recs: [{
-                    back: ['white', 'light', 'x-light']
+                    back: ['white', 'back', 'x-back']
                 }]
             }],
             back: [{
                 title: 'Secondary Background',
                 description: 'A lighter tint of the secondary base color used for backgrounds.',
                 recs: [{
-                    back: ['dark', 'x-dark', 'black']
+                    back: ['dark', 'black']
                 }]
             }],
             'x-back': [{
                 title: 'Secondary Lighter Background',
                 description: 'A very light, near white, tint of the secondary base color for lighter backgrounds.',
                 recs: [{
-                    back: ['dark', 'x-dark', 'black']
+                    back: ['dark', 'black']
                 }]
             }]
         }, {
@@ -82,35 +82,35 @@ define(function() {
                 title: 'Gray Dark',
                 description: 'The darkest shade of the gray color.',
                 recs: [{
-                    back: ['white', 'light', 'x-light']
+                    back: ['white', 'back', 'x-back']
                 }]
             }],
             base: [{
                 title: 'Gray Base',
                 description: 'The base gray color intended for main use.',
                 recs: [{
-                    back: ['white', 'light', 'x-light']
+                    back: ['white', 'back', 'x-back']
                 }]
             }],
             accent: [{
                 title: 'Accent Gray',
                 description: 'The gray accent color.',
                 recs: [{
-                    back: ['white', 'light', 'x-light']
+                    back: ['white', 'back', 'x-back']
                 }]
             }],
             back: [{
                 title: 'Gray Background',
                 description: 'A lighter tint of the gray base color used for backgrounds.',
                 recs: [{
-                    back: ['dark', 'x-dark', 'black']
+                    back: ['dark', 'black']
                 }]
             }],
             'x-back': [{
                 title: 'Gray Lighter Background',
                 description: 'A very light, near white, tint of the gray base color for lighter backgrounds.',
                 recs: [{
-                    back: ['dark', 'x-dark', 'black']
+                    back: ['dark', 'black']
                 }]
             }]
         }, {
@@ -133,7 +133,7 @@ define(function() {
                 title: 'Success Background',
                 description: 'Success messaging background color.',
                 recs: [{
-                    back: ['black', 'x-dark', 'dark']
+                    back: ['black', 'dark']
                 }]
             }]
         }, {
@@ -156,7 +156,7 @@ define(function() {
                 title: 'Error Background',
                 description: 'Error messaging background color.',
                 recs: [{
-                    back: ['black', 'x-dark', 'dark']
+                    back: ['black', 'dark']
                 }]
             }]
         }, {
@@ -172,7 +172,7 @@ define(function() {
                 title: 'Warning Background',
                 description: 'Warning messaging background color.',
                 recs: [{
-                    back: ['black', 'x-dark', 'dark']
+                    back: ['black', 'dark']
                 }]
             }]
         }]
