a5c6e3179891288dfc81f4fdeb9a8d75c7fdb10c
Brian Jacobel <bjacobel@gmail.com>
2016-09-20 15:52:53 -0400

    JSDoc-ify

--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -10,7 +10,8 @@ var gulp = require('gulp'),
     wpConfigFactory = wpConfig.configFactory,
     wpConfigDevServer = wpConfig.devServerConfig;
 
-/* Builds the pattern library doc into /pldoc/public.
+/**
+ * Builds a production bundle of the pattern library doc into /pldoc/public.
  * Runs like NODE_ENV = 'production' so that webpack builds a version where CSS is preloaded and JS is uglified.
  * For hot module reload and live browser updates (ie, a development site), use the webpackdev-server task below
  */
@@ -27,7 +28,8 @@ gulp.task('webpack-prod', ['clean'], function(callback) {
     });
 });
 
-/* Builds the pattern library documentation using webpack and serves it with webpack-dev-server
+/*
+ * Builds the pattern library documentation using webpack and serves it with webpack-dev-server.
  * Reloads modules (including CSS) using Webpack Hot Module Reload, at the cost of flashes of unstyled content
  * Serves from memory, not /pldoc/public, so clean that out first to assets from prod build don't turn up in dev
  * Also enables sourcemaps. Serves with the help of proxying to Jekyll's server - this won't work on its own.
@@ -47,7 +49,9 @@ gulp.task('webpack-dev', ['clean'], function() {
     });
 });
 
-/* Do a webpack build with a custom publicPath. This is necessary because preview sites are served from subdirectories.
+/*
+ * Do a webpack build with a custom publicPath.
+ * This is necessary because preview sites are served from subdirectories.
  * It's a little messy but the alternative is a lot of DNS or adding a lot of complexity to the webpack config.
  */
 gulp.task('webpack-public-path-git-branch', ['clean'], function(callback) {
